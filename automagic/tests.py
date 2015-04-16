0a3b9e30c21f6b2de6280210994bcbe3db08d6d9
EthPyth <EthPyth@users.noreply.github.com>
2015-04-16 15:21:38 +0200

    Update tests.py
    Here is some tests for the AutomagicModel

--- automagic/tests.py
+++ automagic/tests.py
@@ -1,3 +1,21 @@
+from django.db import models
 from django.test import TestCase
+from .models import AutomagicModel
 
-# Create your tests here.
+
+class MyFooModel(AutomagicModel):
+    title = models.CharField(max_length=20)
+
+    def __unicode__(self):
+        return self.title
+
+class AutomagicModelTest(TestCase):
+
+    def test_automagic_model(self):
+        self.assertTrue(MyFooModel.objects.create(title='Foobar'))
+
+    def test_get_canonical_slug(self):
+        foo_url = MyFooModel.objects.create(title='Foobar')
+        self.assertEquals(foo_url.get_canonical_slug(), 'foobar')
+        self.assertTrue(isinstance(foo_url.get_canonical_slug(), str), True)
+        self.assertTrue(isinstance(foo_url.get_fields_and_values(), list))
